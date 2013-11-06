f7d230c7a1d69b4a52d1b581cb74d4aa5434dc23
Tom Willemse <tom@ryuslash.org>
2013-11-06 23:29:44 +0100

    Bump version

--- hypo.el
+++ hypo.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tom Willemse <tom@ryuslash.org>
 ;; Keywords: tools
-;; Version: 0.1.0.3
+;; Version: 0.2.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
